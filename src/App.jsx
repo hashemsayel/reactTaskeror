945aeb9f3b6b63eec8a1bc4110463ec845fdbdf3
hashemsayel <hashemsayel4@gmail.com>
2024-03-13 14:27:50 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from './components/Navbar/Navbar'
+import About from './components/About/About'
+import Footer from './components/Footer/Footer'
+
+function App() {
+  return (
+
+    <>
+    <div>
+      <Navbar />
+      <About />
+      <Footer />
+    </div>
+    </>
+
+  )
+}
+
+export default App;
